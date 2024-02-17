9fb435b1d418531ffef8fe24337fe28cd3996f72
Black-Flights <159639324+Black-Flights@users.noreply.github.com>
2024-02-18 00:43:14 +0530

    data

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
-
+const fs = require('fs')
 const port = process.env.PORT || 3000
+
+const dataURL = './data.json'
+
 
 app.get( "/", ( req, res) => {
     res.end("Running")
 })
+
+
+app.get("/data", (req, res) => [
+
+    fs.readFile( dataURL, ( err, data) => {
+        if(!err){
+            res.end(data);
+        }else{
+            res.end("Something wrong")
+        }
+    })
+
+])
+
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
